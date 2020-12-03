b1aa9344b1f315907228b2acf10018fec09c9286
megamad <syav.popoff@yandex.ru>
2020-12-03 22:50:20 +0300

    Исправил синтаксис согласно линтеру

--- app.r
+++ app.r
@@ -1,48 +1,48 @@
 ﻿library(shiny)
 library(stringr)
-source('src/moex.r')
-source('src/stocks_toolbox.r')
+source("src/moex.r")
+source("src/stocks_toolbox.r")
 
-if (.Platform$OS.type == "windows"){
-  Sys.setlocale(locale = 'Russian')
+if (.Platform$OS.type == "windows") {
+  Sys.setlocale(locale = "Russian")
 }
 
-moex = MoexApi$new(config$moex)
-all_sec_df = moex$get_all_securities()
+moex <- MoexApi$new(config$moex)
+all_sec_df <- moex$get_all_securities()
 
 ui <- fluidPage(
-  title = 'Stocks analyzer',
+  title = "Stocks analyzer",
   sidebarLayout(
     sidebarPanel(
-      selectInput('sec', 'Ticker symbol', choices = c("", all_sec_df$secid)),
+      selectInput("sec", "Ticker symbol", choices = c("", all_sec_df$secid)),
       conditionalPanel(
         condition = "input.sec != ''",
-        selectInput('board_id', 'Board ID', choices = "")
+        selectInput("board_id", "Board ID", choices = "")
       ),
       hr(),
-      checkboxGroupInput('ta', 'Select TA', choices = c('sma_ta') )
+      checkboxGroupInput("ta", "Select TA", choices = c("sma_ta", "ema_ta"))
     ),
     mainPanel(
-      textOutput('sec_title'),
-      highchartOutput('candles')
+      textOutput("sec_title"),
+      highchartOutput("candles")
     )
   )
 )
 
-server <- function(input, output, session){
+server <- function(input, output, session) {
 
   reactive_sec_history <- reactive({
-    validate(need(input$sec, 'Select security id'))
-    moex$sec_history(engine = 'stock', market = 'shares', sec = input$sec)
+    validate(need(input$sec, "Select security id"))
+    moex$sec_history(engine = "stock", market = "shares", sec = input$sec)
   })
 
   observe({
-    ids = unique(reactive_sec_history()$BOARDID)
-    updateSelectInput(session, 'board_id', choices = c('', ids))
+    ids <- unique(reactive_sec_history()$BOARDID)
+    updateSelectInput(session, "board_id", choices = c("", ids))
   })
 
   reactive_data <- reactive({
-    validate(need(input$board_id, 'Select board id'))
+    validate(need(input$board_id, "Select board id"))
     reactive_sec_history() %>%
       filter(BOARDID == input$board_id) %>%
       select(-BOARDID, -SHORTNAME, -SECID) %>%
@@ -51,7 +51,8 @@ server <- function(input, output, session){
 
   output$candles <- renderHighchart({
     plot_candles(reactive_data()) %>%
-      purrr::when('sma_ta' %in% input$ta ~ plot_SMA_TA(., reactive_data()), ~ .)
+      purrr::when("sma_ta" %in% input$ta ~ plot_sma_ta(., reactive_data()), ~ .) %>%
+      purrr::when("ema_ta" %in% input$ta ~ plot_ema_ta(., reactive_data()), ~ .)
   })
 
   output$sec_title <- renderText({
--- src/stocks_toolbox.r
+++ src/stocks_toolbox.r
@@ -7,27 +7,27 @@ library(highcharter)
 
 
 # Tim Tillson's T3 indicator
-T3 <- function(x, n=10, v=1) DEMA(DEMA(DEMA(x,n,v),n,v),n,v)
+t3 <- function(x, n=10, v=1) DEMA(DEMA(DEMA(x, n, v), n, v), n, v)
 
-plot_candles <- function(df){
-  highchart(type = 'stock') %>%
+plot_candles <- function(df) {
+  highchart(type = "stock") %>%
     hc_add_series(xts::as.xts(df))
 }
 
 # indicators
-add_SMA_TA <- function(df, short_run = 30, long_run = 200){
+add_sma_ta <- function(df, short_run = 30, long_run = 200) {
   df %>%
     mutate(sma_short = SMA(CLOSE, n = short_run),
            sma_long = SMA(CLOSE, n = long_run))
 }
 
-plot_SMA_TA <- function(chart, df, short_run = 30, long_run = 200){
-  df = add_SMA_TA(df, short_run, long_run)
+plot_sma_ta <- function(chart, df, short_run = 30, long_run = 200) {
+  df <- add_SMA_TA(df, short_run, long_run)
   chart %>%
     hc_add_series(df, hcaes(y = sma_short, x = TRADEDATE),
-                  type = 'line', name = str_glue('SMA {short_run} days')) %>%
+                  type = "line", name = str_glue("SMA {short_run} days")) %>%
     hc_add_series(df, hcaes(y = sma_long, x = TRADEDATE),
-                  type = 'line', name = str_glue('SMA {long_run} days'))
+                  type = "line", name = str_glue("SMA {long_run} days"))
 }
 
 # signals
